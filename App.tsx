39736c0106eeb14d199042392e77dc5cd48f8b0f
HaritsN <haritsnuragapadika@mail.ugm.ac.id>
2024-11-04 02:57:19 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -76,18 +76,19 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="Nama">
+           Harits Nuraga Padika
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
+          <Section title="NIM">
+            22/494436/SV/20823
           </Section>
-          <Section title="Debug">
-            <DebugInstructions />
+          <Section title="Kelas">
+           PGPBL A
           </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
+          <Section title="Link Media Sosial">
+           
+
+           
           </Section>
           <LearnMoreLinks />
         </View>
